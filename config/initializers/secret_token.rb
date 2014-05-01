9be06aef0ae8207a7a08907afbf3bd42392effef
fedetaglia <fede.tagliabue@gmail.com>
2014-05-01 14:32:39 +1000

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+MyFbPicts::Application.config.secret_key_base = '39a9de0632b87ff6e1aca2888daa58974dfcc48b41bc697dc294b29f822fd45ae2412b0461c0f606c7a1d1da11eaf31408e84bb7e1458ccc4311dd8f8005ca85'
