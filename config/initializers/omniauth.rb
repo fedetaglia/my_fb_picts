e6c4e974774c83e32bdeff4b1eaaea54f5a9e86d
fedetaglia <fede.tagliabue@gmail.com>
2014-05-13 23:34:26 +1000

    fix facebook authorization

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,6 +1,7 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
 
-  provider :facebook, ENV["FB_APP_ID"], ENV["FB_APP_SECRET"]
+  provider :facebook, ENV["FB_APP_ID"], ENV["FB_APP_SECRET"],
+            scope: 'public_profile,user_photos'
   
   provider :instagram, ENV['IG_APP_ID'], ENV['IG_APP_SECRET']
 
